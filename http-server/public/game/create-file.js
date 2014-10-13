cceed1ca2f3c0ed96c1c59468126f2b9317a467f
jamesjacko <james.ogge@gmail.com>
2014-10-13 15:54:36 +0100

    starts on steering

--- http-server/public/game/create-file.js
+++ http-server/public/game/create-file.js
@@ -63,7 +63,7 @@ function createJSON(worldWidth, worldHeight){
 
     for(i = 0; i < world.width * world.height; i++){
         if(world.layers[0].data[i] == 1){
-            ask({prob:1, func:sea, params:i});
+           ask({prob:1, func:sea, params:i});
         }
     }
 
@@ -97,7 +97,7 @@ function createJSON(worldWidth, worldHeight){
     }
 
     var json = JSON.stringify(world);
-    console.log(world);
+
     var blob = new Blob([json], {type: "application/json"});
     var url  = URL.createObjectURL(blob);
 
--- http-server/public/game/script.js
+++ http-server/public/game/script.js
@@ -7,3 +7,16 @@ function ask(options){
     if(Math.floor(Math.random() * options.prob) == 0)
         options.func(options.params);
 }
+
+function vect_magnitude(vector){
+  vector.magnitude = Math.sqrt(Math.pow(vector.x, 2) + Math.pow(vector.y, 2));
+}
+
+function vect_normalize(vector){
+  if(vector.magnitude){
+    vector.normal = {
+      x: vector.x * vector.magnitude,
+      y: vector.y * vector.magnitude
+    };
+  }
+}
